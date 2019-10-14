68f87b11200905de8c4f7b8996db41af186a5864
Artemichi <55092560+Artemichi@users.noreply.github.com>
2019-10-14 12:37:19 +0500

    change

--- sketch.js
+++ sketch.js
@@ -2,22 +2,23 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight)
 	angleMode(DEGREES)
-	n = 6
-	d = 72
+	n = 8
+	d = 67
+	a = 0
 	scale = width * 0.2
 }
 
 function draw() {
 
 	background(0)
+	
 	translate(width/2, height/2)
-
+	rotate(a)
 	stroke(222,0,0)
-	strokeWeight(0.5)
 	noFill()
 
 	beginShape()
-	for (let i = 0; i < 722; i++) {
+	for (let i = 0; i < 361; i++) {
 		let k = i * d
 		let r = scale * sin(n*k)
 		let x = r * cos(k)
@@ -26,8 +27,10 @@ function draw() {
 	}
 	endShape(CLOSE)
 
-	n+=0.0000001
+	n+=0.000001
 	d+=0.0001
+	
+	a += 0.1
 }
 
 function windowResized() {
