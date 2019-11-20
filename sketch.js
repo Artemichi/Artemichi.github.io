b0baeb5b5ad581760b07dd1ab0764251c57adef0
Artemichi <55092560+Artemichi@users.noreply.github.com>
2019-11-20 18:56:46 +0500

    mutation refactor

--- sketch.js
+++ sketch.js
@@ -1,38 +1,90 @@
-
 function setup() {
 	createCanvas(windowWidth, windowHeight)
 	angleMode(DEGREES)
-	n = 6
-	d = 72
+	n = 3
+	d = 29.55
+	na = 0.0001
+	da = 0.001
 	a = 0
-	scale = width * 0.2
+
+	if (window.innerWidth == 320) {
+		vScale = 0.5
+	} else if (window.innerWidth < 767) {
+		vScale = 0.6
+	} else if (window.innerWidth < 1199) {
+		vScale = 0.9
+	} else {
+		vScale = 1
+	}
+	invert = false
+	alp = 100
+
+	colors = [{
+			r: 38,
+			g: 70,
+			b: 83
+		},
+		{
+			r: 42,
+			g: 157,
+			b: 143
+		},
+		{
+			r: 231,
+			g: 200,
+			b: 10
+		},
+		{
+			r: 21,
+			g: 20,
+			b: 110
+		}
+	]
 }
 
 function draw() {
-
 	background(0)
-	
-	translate(width/2, height/2)
-	rotate(a)
-	stroke(222,0,0)
-	noFill()
+	translate(width / 2, height / 2)
 
-	beginShape()
-	for (let i = 0; i < 361; i++) {
-		let k = i * d
-		let r = scale * sin(n*k)
-		let x = r * cos(k)
-		let y = r * sin(k)
-		vertex(x, y)
+	for (let i = 0; i < 4; i++) {
+		rotate(a + i)
+		stroke(
+			colors[i].r,
+			colors[i].g,
+			colors[i].b,
+			alp)
+		noFill()
+		beginShape()
+		for (let i = 0; i < 300; i++) {
+			let k = i * d
+			let r = i * sin(n * k) * vScale
+			let x = r * cos(k)
+			let y = r * sin(k)
+			vertex(x, y)
+		}
+		endShape(CLOSE)
 	}
-	endShape(CLOSE)
 
-	n+=random(0.0001)
-	d+=random(0.001)
-	
+	if (n > 3.037) {
+		invert = true
+	}
+
+	if (n < 3) {
+		invert = false
+	}
+
+	if (!invert) {
+		n += na
+		d += da
+	} else {
+		n -= na
+		d -= da
+	}
 	a += 0.1
 }
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight)
+	setup()
+	draw()
 }
