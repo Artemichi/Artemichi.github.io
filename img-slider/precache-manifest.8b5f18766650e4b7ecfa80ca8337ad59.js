4f322b452d793ed15a95e8588cac9b4abfd62d97
Artemichi <55092560+Artemichi@users.noreply.github.com>
2020-04-12 00:17:07 +0500

    img-slider

---
+++ img-slider/precache-manifest.8b5f18766650e4b7ecfa80ca8337ad59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477bf6afcb6eb0f617d73fbdbd0f7b3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e8976c0684d11f756a2",
+    "url": "/static/css/main.d6c6cfd5.chunk.css"
+  },
+  {
+    "revision": "31f68f8186cbb45752c0",
+    "url": "/static/js/2.b8477bcd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b8477bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8976c0684d11f756a2",
+    "url": "/static/js/main.3f7015da.chunk.js"
+  },
+  {
+    "revision": "f8e4bb72e6f09e68387d",
+    "url": "/static/js/runtime-main.684652d9.js"
+  }
+]);
